1e5a75e5d06339dfb211a783fd2b602b3c1063fb
naikymen <you@example.com>
2024-06-03 20:09:15 -0300

    init

---
+++ R/reprex.R
@@ -0,0 +1,16 @@
+#' A reprex
+#' 
+#' A reprex
+#'
+#' @name reprex
+#' 
+#' @seealso \link{magick}
+"_PACKAGE"
+
+#' Test
+#'
+#' @export
+#' @importFrom IRanges IRanges
+test_reprex <- function () {
+  IRanges::IRanges()
+}
